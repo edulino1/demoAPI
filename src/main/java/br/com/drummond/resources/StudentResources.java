bfce8934a4529a964614218f04e6e3660cda0d78
edulino1 <edu.lino1@uol.com.br>
2019-10-30 21:45:30 -0300

    @CrossOrigin

--- src/main/java/br/com/drummond/resources/StudentResources.java
+++ src/main/java/br/com/drummond/resources/StudentResources.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.drummond.domain.Student;
 import br.com.drummond.repository.StudentRepository;
 
+@CrossOrigin
 @RestController
 public class StudentResources {
 
