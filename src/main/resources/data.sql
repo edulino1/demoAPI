eff1a9fbbdfbe1e8ef0388a3ac923e6d14fbec11
edulino1 <37522461+edulino1@users.noreply.github.com>
2019-11-06 20:00:46 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ insert into student (ra, name, email) values (172000011,'Eduardo Lino', 'edu.lin
 insert into student (ra, name, email) values (1720,'Rosa Aparecida', 'rosa@gmail.com');
 insert into student (ra, name, email) values (1234,'Maria Eduarda', 'mduda@gmail.com');
 insert into student (ra, name, email) values (1235,'Letícia Rodrigues', 'letrodr@uol.com.br');
-insert into student (ra, name, email) values (1235,'Juan Oliveira', 'juanoliv@uol.com.br');
-insert into student (ra, name, email) values (1235,'Isaac Newton', 'isnew@gmail.com');
+insert into student (ra, name, email) values (1236,'Juan Oliveira', 'juanoliv@uol.com.br');
+insert into student (ra, name, email) values (1237,'Isaac Newton', 'isnew@gmail.com');
